c81d93fd2e2eca63fe973abdfa46aadfe7fb2392
Orlando <orlandodaw@gmail.com>
2017-12-18 02:26:01 -0600

    send mail done

--- send.php
+++ send.php
@@ -1,12 +1,47 @@
 <?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load composer's autoloader
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 1;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'mailCreurmack';                 // SMTP username
+    $mail->Password = 'passwordCreurmack';                           // SMTP password
+    //$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('mailCreurmack', 'Mailer');
+    $mail->addAddress('mailCreurmack', 'Creurmack');     // Add a recipient
+    //$mail->addAddress('ellen@example.com');               // Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Contacto';
+    $mail->Body    = $_POST["name"].'   </b>'.$_POST["mail"].'   </b>'.$_POST["phone"];
+    $mail->AltBody = 'Mensaje vacío';
   
-  $destination= "ventas@creurmack.mx";
-  $nombre= $_POST["nombre"];
-  $mail= $_POST["mail"];
-  $phone= $_POST["phone"];
-  $contenido= "Nombre: " . $nombre . "\nCorreo " . $mail . "\nTeléfono " . $phone;
 
-  mail($destination,"Contacto",$contenido);
-  header("Location:thanks.html");
-
-?>
+    $mail->send();
+    header("Location:thanks.html");
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Mensaje no enviado';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
